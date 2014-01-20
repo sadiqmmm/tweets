0832879441d36f244eda36181abad6cf500a48a5
Mohammed Sadiq <sadiqmmm@gmail.com>
2014-01-20 17:22:41 +0530

    Add Tweets public page

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -1,10 +1,15 @@
 class TweetsController < ApplicationController
+
+	def index
+		@tweets = Tweet.all
+		@tweet = Tweet.new
+	end
+
 	def create
 		tweet = Tweet.new(params[:tweet])
 		tweet.user_id = current_user.id
  
 		flash[:error] = "Your Tweet was over 140 characters" unless tweet.save
-		redirect_to current_user
-		 
+		redirect_to current_user		 
 	end
 end
