4cb49945ced63f810f19a0e18da09615044da4c4
Mohammed Sadiq <sadiqmmm@gmail.com>
2014-01-20 16:59:33 +0530

    Add ability to save tweets

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -1,2 +1,10 @@
 class TweetsController < ApplicationController
+	def create
+		tweet = Tweet.new(params[:tweet])
+		tweet.user_id = current_user.id
+ 
+		flash[:error] = "Your Tweet was over 140 characters" unless tweet.save
+		redirect_to current_user
+		 
+	end
 end
